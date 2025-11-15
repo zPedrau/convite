7520764f759f6dfe569287f09b3d031149811340
Pedro Gabriel Matias Manzi <pedrgabriel.manzi@edu.uniube.br>
2025-11-15 03:36:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function fugir() {
+    const btnNao = document.getElementById("btn-nao");
+    const larguraJanela = window.innerWidth;
+    const alturaJanela = window.innerHeight;
+    btnNao.style.position = "fixed"; 
+    
+    const novaEsquerda = Math.random() * (larguraJanela - 100);
+    const novaTopo = Math.random() * (alturaJanela - 50);
+
+    btnNao.style.left = novaEsquerda + "px";
+    btnNao.style.top = novaTopo + "px";
+}
+function aceitar() {
+    confetti({
+        particleCount: 150,
+        spread: 70,
+        origin: { y: 0.6 },
+        colors: ['#81c784', '#ff7043', '#4caf50']
+    });
+    document.querySelector('.btn-container').style.display = 'none';
+    document.getElementById('mensagem-final').style.display = 'block';
+    setTimeout(() => {
+        const seuNumero = "+55 34987213997"; 
+        const mensagem = encodeURIComponent("Oi! Eu aceitei o convite nutricional! Quando vamos sair? 🥑");
+        window.location.href = `https://wa.me/${seuNumero}?text=${mensagem}`;
+    }, 2500);
+}
